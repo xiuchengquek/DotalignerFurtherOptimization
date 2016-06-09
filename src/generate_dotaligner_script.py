e0980f3c42873bffeae9f89bf2e3561a18d55a89
xiuchengquek <biobirdman@gmail.com>
2016-06-09 11:39:00 +1000

    add prefix options

--- src/generate_dotaligner_script.py
+++ src/generate_dotaligner_script.py
@@ -12,6 +12,8 @@ parser = argparse.ArgumentParser(description='Dotaligner Wrapper - Current only 
 parser.add_argument('-s' , '--seq' , help = "pairwise alignment list", required=True)
 parser.add_argument('-p' , '--para' , help = "parameter files", required=True)
 parser.add_argument('-d' , '--dotaligner', help = 'path to the dotaligner', required=True)
+parser.add_argument('-d' , '--prefix', help = 'directory prefix')
+
 
 
 
@@ -25,10 +27,16 @@ if __name__ == '__main__' :
     parameters = parse_param_file(args.para)
     pairs =  parse_pairwise_alignments(args.seq)
     dotaligner = args.dotaligner
+
+
+
+
     directory = 'src/dotaligner_shell_scripts/'
     if not os.path.exists(directory):
         os.makedirs(directory)
     da = DotAlignerGrouped(dotaligner, directory)
+    if args.prefix :
+        da.add_prefix_pairs(args.prefix)
     da.add_pairs(pairs)
     da.add_prefix_pairs('data/ps')
     da.update_parameters(parameters)
